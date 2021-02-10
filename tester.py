b55ea7a6acbc6a7253e1275dbc965aedc1ebbac7
Brandon Ching <68457375+brandonching@users.noreply.github.com>
2021-02-10 11:25:31 -0700

    Restructure Folders

---
+++ tester.py
@@ -0,0 +1,10 @@
+# Brandon Ching
+# CSCI102 - Section C
+# Python-RunicTunes
+
+english_word = input()
+runic_word = ""
+for i in english_word:
+    if (i != 'p'):
+        runic_word = runic_word + i
+print(runic_word)        
