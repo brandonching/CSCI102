8d2d200032bc36cff0f06616cf416ee68db5cdbf
Brandon Ching <68457375+brandonching@users.noreply.github.com>
2021-03-03 11:06:48 -0700

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,10 +1,2 @@
-# Brandon Ching
-# CSCI102 - Section C
-# Python-RunicTunes
-
-english_word = input()
-runic_word = ""
-for i in english_word:
-    if (i != 'p'):
-        runic_word = runic_word + i
-print(runic_word)        
+name = "tanner"
+print(name[-1])
